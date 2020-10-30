8f773f7b44b8b0354fc450a460a966b947511a93
wandella maia <wandella.maia@dtidigital.com.br>
2020-10-29 23:29:02 -0300

    Inserindo imagens no back

--- src/server.js
+++ src/server.js
@@ -5,11 +5,15 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json({
+    limit: '50mb'
+  }));
+  
+//app.use(bodyParser.urlencoded({limit: '200mb', extended: true, parameterLimit:50000}));
 
 require('./controllers/authController')(app);
 require('./controllers/storyController')(app);
 require('./controllers/registerController')(app);
+require('./controllers/imageController')(app);
 
 app.listen(3000);
